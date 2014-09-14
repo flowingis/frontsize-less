62eef2bc719d919772217671394848154e8e85fa
Vittorio Vittori <me@vittoriovittori.com>
2014-09-14 16:51:13 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function(grunt) {
                 ],
                 tasks: [
                     "less:develop",
-                    "test",
+                    "production",
                     "assets"
                 ]
             }
